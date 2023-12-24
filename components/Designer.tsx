a194eb6b3da790fb14eccd81054385c004c0ed0c
Mario Cesena <112787277+mariocc22@users.noreply.github.com>
2023-12-23 23:26:39 -0800

    feat: Adding the draggable functionality

--- components/Designer.tsx
+++ components/Designer.tsx
@@ -3,7 +3,12 @@ import React, { useState } from "react";
 import DesginerSidebar from "./DesginerSidebar";
 
 // Droppable
-import { DragEndEvent, useDndMonitor, useDroppable } from "@dnd-kit/core";
+import {
+  DragEndEvent,
+  useDndMonitor,
+  useDraggable,
+  useDroppable,
+} from "@dnd-kit/core";
 import { cn } from "@/lib/utils";
 import {
   ElementsType,
@@ -12,6 +17,8 @@ import {
 } from "./FormElements";
 import useDesigner from "./hooks/useDesigner";
 import { idGenerator } from "@/lib/idGenerator";
+import { Button } from "./ui/button";
+import { BiSolidTrash } from "react-icons/bi";
 
 function Designer() {
   const { elements, addElement } = useDesigner();
@@ -56,7 +63,7 @@ function Designer() {
               Drop Here!
             </p>
           )}
-          {droppable.isOver && (
+          {droppable.isOver && elements.length === 0 && (
             <div className="p-4 w-full">
               <div className="h-[120px] rounded-md bg-primary/20"></div>
             </div>
@@ -76,6 +83,9 @@ function Designer() {
 }
 
 function DesignerElementWrapper({ element }: { element: FormElementInstance }) {
+  const { removeElement } = useDesigner();
+  const [mouseIsOver, setMouseIsOver] = useState<Boolean>(false);
+
   const topHalf = useDroppable({
     id: element.id + "-top",
     data: {
@@ -94,10 +104,72 @@ function DesignerElementWrapper({ element }: { element: FormElementInstance }) {
     },
   });
 
+  const draggable = useDraggable({
+    id: element.id + "-drag-handler",
+    data: {
+      type: element.type,
+      elementId: element.id,
+      isDesignerElement: true,
+    },
+  });
+
+  if (draggable.isDragging) return null;
+
   const DesignerElement = FormElements[element.type].designerComponent;
   return (
-    <div className="flex w-full h-[120px] items-center rounded-md bg-accent/40 px-4 py-2 pointer-events-none">
-      <DesignerElement elementInstance={element} />
+    <div
+      ref={draggable.setNodeRef}
+      {...draggable.attributes}
+      {...draggable.listeners}
+      className="relative h-[120px] flex flex-col text-foreground hover:cursor-pointer rounded-md ring-1 ring-accent ring-inset"
+      onMouseEnter={() => setMouseIsOver(true)}
+      onMouseLeave={() => setMouseIsOver(false)}
+      onClick={(e) => {
+        e.stopPropagation();
+      }}
+    >
+      <div
+        ref={topHalf.setNodeRef}
+        className="absolute  w-full h-1/2 rounded-t-md"
+      />
+      <div
+        ref={bottomHalf.setNodeRef}
+        className="absolute  w-full bottom-0 h-1/2 rounded-b-md"
+      />
+      {mouseIsOver && (
+        <>
+          <div className="absolute right-0 h-full">
+            <Button
+              className="flex justify-center h-full border rounded-md rounded-l-none bg-red-500"
+              variant={"outline"}
+              onClick={() => {
+                removeElement(element.id);
+              }}
+            >
+              <BiSolidTrash className="h-6 w-6" />
+            </Button>
+          </div>
+          <div className="absolute top-1/2 left-1/2  -translate-x-1/2 -translate-y-1/2 animate-pulse">
+            <p className="text-muted-foreground text-sm">
+              Click for properties or drag to move
+            </p>
+          </div>
+        </>
+      )}
+      {topHalf.isOver && (
+        <div className="absolute top-0 w-full rounded-md h-[7px] bg-primary rounded-b-none" />
+      )}
+      <div
+        className={cn(
+          "flex w-full h-[120px] items-center rounded-md bg-accent/40 px-4 py-2 pointer-events-none opacity-100",
+          mouseIsOver && "opacity-30"
+        )}
+      >
+        <DesignerElement elementInstance={element} />
+      </div>
+      {bottomHalf.isOver && (
+        <div className="absolute bottom-0 w-full rounded-md h-[7px] bg-primary rounded-t-none" />
+      )}
     </div>
   );
 }
